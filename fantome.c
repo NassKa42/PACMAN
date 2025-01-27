89c411f95cf73fdb8812253097cef57ff88da1bc
nas <nassim.kandili@gmail.com>
2025-01-27 16:36:47 +0100

    fantome1

--- fantome.c
+++ fantome.c
@@ -1,6 +1,6 @@
 #include "ressources.h"
 #include "fantome.h"
-
+#include "plateau.h"
 
 fantome initFantome(){
     fantome name;
@@ -12,7 +12,16 @@ fantome initFantome(){
 
 
 char deplacement_fantome_rng(fantome ghost,plateau map){
-        
+    int collision = 0;
+    int deplace = rand()%2;
+    
+    if ((ghost.dir == "h" && map.tab[ghost.posY-1][ghost.posX] == 1) || (ghost.dir == "b" && map.tab[ghost.posY+1][ghost.posX] == 1)|| (ghost.dir == "g" && map.tab[ghost.posY][ghost.posX-1] == 1)|| (ghost.dir == "d" && map.tab[ghost.posY][ghost.posX+1] == 1))
+    {
+            collision = 1;
+    }
+    
+    
+    
 
 }
 
