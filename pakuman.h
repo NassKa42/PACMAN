bcc7fb91b12e370aba817ef3bbafc55f23da6a37
Nas <104835408+NassKa42@users.noreply.github.com>
2025-02-03 13:20:02 +0100

    Add files via upload

---
+++ pakuman.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <string.h>
+
+//0: vide
+//1: mur
+//2: gum
+//3: mega gum
+//4: fruit
+//5: pacman
+//6: rouge
+//7: orange
+//8: bleu
+//9: rose
+
+#ifndef PatoisDuNord
+#define PatoisDunord
+
+struct PlayerStruct{char name; int Pos_X; int Pos_Y; int orientation; int score;};
+typedef struct PlayerStruct Player;
+Player initPlayer(char* name);
+int bouger(char direction, Player pakuman);
+Player bougerdirect(char direction, Player pakuman);
+
+#endif
