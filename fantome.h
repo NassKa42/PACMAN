445551c64d7888a5dd73be13862cbfd50605232a
nas <nassim.kandili@gmail.com>
2025-02-10 16:01:50 +0100

    clean

--- fantome.h
+++ fantome.h
@@ -21,13 +21,12 @@ typedef struct StructFant fantome;
 typedef struct POS position;
 fantome initFantome(int x, int y, char name[10]);
 fantome reset_fantome(fantome ghost,int x,int y);
-// fantome deplacement_fantome_rng(fantome ghost,plateau map);
 void caracfantome(fantome ghost);
 fantome deplacement_fantome_proche(Player pacman, fantome ghost, plateau map);
 fantome deplacement_fantome_rng_intersection(fantome ghost, plateau map);
 fantome deplacement_fantome_proche_continue(Player pacman, fantome ghost, plateau map);
 fantome deplacement_fantome_proche_BFS(Player paku,fantome fantt, plateau map);
-// fantome deplacement_fantome_proche2(Player pacman, fantome ghost, plateau map);
+fantome deplacement_fantome_proche_BFS_avant(Player paku,fantome fantt, char dir, plateau map);
 int fantome_peut_avancer(fantome ghost, plateau map);
 int is_collision_p2g(fantome ghost, Player pacman);
 
